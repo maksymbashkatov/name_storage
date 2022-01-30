de3f82bfaa6e1e0055eea8d28752c319f66881a1
Максим Башкатов <maksymbashkatov@gmail.com>
2022-01-30 15:32:59 +0200

    Model Name was created.

--- name_collector/models.py
+++ name_collector/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Name(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'pk: {self.pk} | {self.name}'
+
+    class Meta:
+        db_table = 'Names'
